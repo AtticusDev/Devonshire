6d3a6c773ecb23fdc087f9af6350fd0986325f67
david roland <david.roland@atticuscreative.co.uk>
2019-03-04 14:49:27 +0000

    format the 404 page

--- 404.php
+++ 404.php
@@ -9,58 +9,17 @@
 
 get_header(); ?>
 
-	<div class="container">
-		<div class="row">
-			<div id="primary" class="content-area">
-				<main id="main" class="site-main" role="main">
+	<div class="container mt-5">
+		<div class="row justify-content-center">
 
-					<section class="error-404 not-found">
-						<header class="page-header">
-							<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'devonshire' ); ?></h1>
-						</header><!-- .page-header -->
-
-						<div class="page-content">
-							<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'devonshire' ); ?></p>
-
-							<?php
-								get_search_form();
-
-								the_widget( 'WP_Widget_Recent_Posts' );
-
-								// Only show the widget if site has multiple categories.
-								if ( devonshire_categorized_blog() ) :
-							?>
-
-							<div class="widget widget_categories">
-								<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'devonshire' ); ?></h2>
-								<ul>
-								<?php
-									wp_list_categories( array(
-										'orderby'    => 'count',
-										'order'      => 'DESC',
-										'show_count' => 1,
-										'title_li'   => '',
-										'number'     => 10,
-									) );
-								?>
-								</ul>
-							</div><!-- .widget -->
-
-							<?php
-								endif;
-
-								/* translators: %1$s: smiley */
-								$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'devonshire' ), convert_smilies( ':)' ) ) . '</p>';
-								the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-								the_widget( 'WP_Widget_Tag_Cloud' );
-							?>
+						<div class="col-md-6 col-sm-12 mt-5 mb-5">
+							<h1 class="underline-gold">It looks like nothing was found at this location.</h1>
+							<h3 class="mt-3">Please use the search button or the navigation button in the header to find what you are looking for.</h3>
+								<a href="/" class="btn smallGoldWhiteBtn">Return to home</a>
 
 						</div><!-- .page-content -->
-					</section><!-- .error-404 -->
 
-				</main><!-- #main -->
-			</div><!-- #primary -->
+
 		</div><!--  .row -->
 	</div><!--  .container -->
 
